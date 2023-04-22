42f5bf5473e33c5662fa19c286c5f6af584538c1
lich <100108350@qq.com>
2023-04-22 23:43:51 +0800

    ci: update presetConfig

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,32 @@ module.exports = {
   branches: ["main"], // 指定在哪个分支下要执行发布操作
   plugins: [
     "@semantic-release/commit-analyzer", // 解析 commit 信息，默认就是 Angular 规范
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        "parserOpts": {
+          "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        },
+        "presetConfig": {
+          "types": [
+            {
+              "type": "breaking",
+              "section": "❗ Breaking ❗",
+              "hidden": false
+            },
+            { "type": "feat", "section": "✨ Feature ✨", "hidden": false },
+            { "type": "fix", "section": "🐛 Bugfix 🐛", "hidden": false },
+            { "type": "hotfix", "section": "🔥 Hotfix 🔥", "hidden": false },
+            {
+              "type": "BREAKING",
+              "section": "❗ Breaking ❗",
+              "hidden": false
+            },
+            { "type": "HOTFIX", "section": "🔥 Hotfix 🔥", "hidden": false }
+          ]
+        }
+      }
+    ],
     [
       "@semantic-release/changelog",
       {
